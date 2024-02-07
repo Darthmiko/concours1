4144efef8e8a2e7768182a84b40f09f74ccb87b4
szychowskim <szychowski@educ.college-montreal.qc.ca>
2024-02-07 16:14:16 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+Grandes_boites = int(input())
+Petites_boites = int(input())
+Éleves = 28
+petits_gateaux_restants =(8*Grandes_boites + 3*Petites_boites) - Éleves
+print(petits_gateaux_restants)
+
+#2
+B = int(input())
+
+if B == 100:
+    sea_level = 0
+elif B > 100:
+    sea_level = - 1
+elif B < 100:
+    sea_level = 1
+
+print((5 * B) - 400)
+print(sea_level)
+
+#3
